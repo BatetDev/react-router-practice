ba18a1a98ff8a15935774e4535e1c3fe5a8bb1b2
fr4nbtt <batet.francisco@gmail.com>
2026-09-17 23:13:14 -0300

    feat: add nested routed with outlet

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import App from './App.jsx';
 import Profile from './components/Profile.jsx';
+import Popeye from './components/Popeye.jsx';
+import Spinach from './components/Spinach.jsx';
 import './index.css';
 
 const router = createBrowserRouter([
@@ -13,11 +15,15 @@ const router = createBrowserRouter([
   {
     path: 'profile',
     element: <Profile />,
+    children: [
+      { path: 'spinach', element: <Spinach /> },
+      { path: 'popeye', element: <Popeye /> },
+    ],
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );

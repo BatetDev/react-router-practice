be0465f9add1fde4ea4accb1832dee94d95a25f7
fr4nbtt <batet.francisco@gmail.com>
2026-09-18 16:44:57 -0300

    refactor: move routes config to routes.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
+import routes from './routes.jsx';
 import App from './App.jsx';
 import Profile from './components/Profile.jsx';
 import ErrorPage from './components/ErrorPage.jsx';

fee2308af888de8bc43f0b2232418026cf2fdcb8
fr4nbtt <batet.francisco@gmail.com>
2026-09-17 23:48:22 -0300

    feat: add dynamic profile routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,22 +3,18 @@ import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
 import App from './App.jsx';
 import Profile from './components/Profile.jsx';
-import Popeye from './components/Popeye.jsx';
-import Spinach from './components/Spinach.jsx';
+import ErrorPage from './components/ErrorPage.jsx';
 import './index.css';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    errorElement: <ErrorPage />,
   },
   {
-    path: 'profile',
+    path: 'profile/:name',
     element: <Profile />,
-    children: [
-      { path: 'spinach', element: <Spinach /> },
-      { path: 'popeye', element: <Popeye /> },
-    ],
   },
 ]);
 

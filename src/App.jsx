3ad2ddd94cf92fc099347579de7b8bca463c8000
fr4nbtt <batet.francisco@gmail.com>
2026-09-22 18:16:33 -0300

    feat(router: navigate programmatically with useNavigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { useNavigate } from 'react-router';
+
 const App = () => {
+  const navigate = useNavigate();
+  const names = ['popeye', 'olive', 'bluto', 'spinach'];
+
+  const goToRandomProfile = () => {
+    const randomName = names[Math.floor(Math.random() * names.length)];
+    navigate(`/profile/${randomName}`);
+  };
+
   return (
     <div>
       <h1>Hello from the main page of the app!</h1>
       <p>Here are some examples of links to other pages</p>
+      <button onClick={goToRandomProfile}>Go to a random profile</button>
     </div>
   );
 };
